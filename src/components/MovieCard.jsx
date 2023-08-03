cc6e00969a86025e8c5e8b7afd09b09c7a023dbb
jamesdai05 <dai_guilong@hotmail.com>
2023-08-04 01:32:08 +0800

    added the content to modal

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
 const api_img = "https://image.tmdb.org/t/p/w500";
 
 const MovieCard = (props) => {
-  console.log(props.poster);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="text-center card bg-secondary mb-3">
       <div className="card-body">
         <div className="card-img-top">
           <img src={api_img + props.poster} alt="movie" />
           <div className="card-body">
-            <button type="button" className="btn btn-dark">
-              View more
-            </button>
+            <Button variant="dark" onClick={handleShow}>
+              View More
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>{props.title}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <img src={api_img + props.poster} alt="movie" />
+                <div className="description">{props.overview}</div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </div>
       </div>
