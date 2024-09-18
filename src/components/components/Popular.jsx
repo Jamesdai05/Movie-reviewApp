2c0364ce8f5a82b015738c2d2631098ba1f0b83c
jamesdai05 <dai_guilong@hotmail.com>
2024-09-18 17:56:02 +0800

    add the pagination to the popular page

--- src/components/components/Popular.jsx
+++ src/components/components/Popular.jsx
@@ -1,11 +1,24 @@
 import { useState, useEffect } from "react";
 import MovieCard from "../MovieCard";
 import ReactPaginate from "react-paginate";
+import axios from "axios";
 
-const apiKey = process.env.REACT_APP_API_KEY;
-const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+// const apiKey = process.env.REACT_APP_API_KEY;
+// const token = process.env.REACT_API_TOKEN;
+// const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 //base of the poster and image.
-// const api_img = "https://image.tmdb.org/t/p/w500";
+const api_img = "https://image.tmdb.org/t/p/w500";
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MmM5MDM1NTdlNWI4ZGQ5ZjM4NmU4Nzc1ZmIxNmIyYiIsIm5iZiI6MTcyNjY0ODU5Ni4wMzU1NzUsInN1YiI6IjY0YzRjM2ZmZWVjNWI1MDExY2IwMWE1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QjqhvJUEPoTJGIsYwDPtITf8StzgPSQl6Kt7bm7I8ps";
+
+const options = {
+  method: "GET",
+  url: "https://api.themoviedb.org/3/movie/popular?language=en-US&page=20",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
 
 const Popular = () => {
   const [populars, setPopulars] = useState([]);
@@ -15,13 +28,27 @@ const Popular = () => {
   const pageVisited = pageNumber * itemsPerPage;
   const LastIndexOfItemPerPage = pageVisited + itemsPerPage;
 
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setPopulars(data.results);
+  //     });
+  // }, []);
+
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setPopulars(data.results);
+    const fetchData = function () {
+      axios(options).then((res) => {
+        console.log(res.data);
+        setPopulars(res.data.results);
       });
+      // response = response.data;
+      // console.log(response.results);
+      // setPopulars(response.results);
+    };
+    fetchData();
+    // console.log(response.data);
   }, []);
 
   const currentItems = populars.slice(pageVisited, LastIndexOfItemPerPage);
@@ -52,15 +79,17 @@ const Popular = () => {
       ) : (
         <h1>Results are not found!Please check the enter is correct.</h1>
       )}
-      <ReactPaginate
-        breakAriaLabels="..."
-        nextAriaLabel={"Next"}
-        previousAriaLabel={"Previous"}
-        pageCount={pageCount}
-        onPageChange={handlePageChange}
-        containerClassName={"paginationBttns"}
-        activeClassName={"paginationActive"}
-      />
+      {currentItems.length > 0 && (
+        <ReactPaginate
+          breakAriaLabels="..."
+          nextAriaLabel={"Next"}
+          previousAriaLabel={"Previous"}
+          pageCount={pageCount}
+          onPageChange={handlePageChange}
+          containerClassName={"paginationBttns"}
+          activeClassName={"paginationActive"}
+        />
+      )}
     </>
   );
 };
